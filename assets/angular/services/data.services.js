c9362f9fb30e6c15b848823dc2eead29ed6bcb7c
Lucas Gonzalo <lucasg.ruizd@gmail.com>
2018-02-26 00:06:39 -0700

    Cambiados valores a directorios en el servidor

--- assets/angular/services/data.services.js
+++ assets/angular/services/data.services.js
@@ -4,19 +4,19 @@ neuData.$inject = ['$http', '$window'];
 
 function neuData($http, $window){
   function getAllPortfolio(){
-    return $http.get('/neuderts/scripts/portfolio/getAllPortfolio.php').then(complete).catch(failed);
+    return $http.get('/scripts/portfolio/getAllPortfolio.php').then(complete).catch(failed);
   }
 
   function getOnePortfolio(slug){
-    return $http.get('/neuderts/scripts/portfolio/getOnePortfolio.php?slug='+slug).then(complete).catch(failed);
+    return $http.get('/scripts/portfolio/getOnePortfolio.php?slug='+slug).then(complete).catch(failed);
   }
 
   function getAllBlog(){
-    return $http.get('/neuderts/scripts/blog/getAllBlog.php').then(complete).catch(failed);
+    return $http.get('/scripts/blog/getAllBlog.php').then(complete).catch(failed);
   }
 
   function getOneBlog(slug){
-    return $http.get('/neuderts/scripts/blog/getOneBlog.php?slug='+slug).then(complete).catch(failed);
+    return $http.get('/scripts/blog/getOneBlog.php?slug='+slug).then(complete).catch(failed);
   }
 
   function complete(response){
