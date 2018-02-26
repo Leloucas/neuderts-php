ee0d7f242df4fcc9cb553bebf020ae5f89317034
Lucas Gonzalo <lucasg.ruizd@gmail.com>
2018-02-25 23:59:11 -0700

    Cambiados valores a valores de prueba

--- scripts/variables.php
+++ scripts/variables.php
@@ -1,7 +1,7 @@
 <?php
     header('Content-Type: application/json; charset=utf-8');
-    $host = 'localhost';
-    $dbname = 'neuderts';
-    $username = 'root';
-    $pass = '';
+    $host = '187.210.148.28';
+    $dbname = 'neuderts_test';
+    $username = 'neuderts_test';
+    $pass = '123456';
  ?>
