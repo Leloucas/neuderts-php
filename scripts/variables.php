a1edeb332fad520be9410f507a2646a77b2c20c3
Lucas <lucasg.ruizd@gmail.com>
2019-02-13 00:12:15 -0700

    Removido variables.php del código fuente

--- scripts/variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Content-Type: application/json; charset=utf-8');
-    $host = 'localhost';
-    $dbname = 'neuderts_server';
-    $username = 'neuderts_neudert';
-    $pass = '12neuderts34';
- ?>
